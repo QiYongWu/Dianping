674f665aa28a9cead850cd9469a3ea09cf62fc6a
Surgical Knife <qiyongw@126.com>
2025-11-11 21:03:06 +0800

    add：使用Redis来实现点赞过后的blog高亮标识

--- src/main/java/com/hmdp/service/IBlogService.java
+++ src/main/java/com/hmdp/service/IBlogService.java
@@ -3,6 +3,8 @@ package com.hmdp.service;
 import com.hmdp.entity.Blog;
 import com.baomidou.mybatisplus.extension.service.IService;
 
+import java.util.List;
+
 /**
  * <p>
  *  服务类
@@ -14,4 +16,6 @@ import com.baomidou.mybatisplus.extension.service.IService;
 public interface IBlogService extends IService<Blog> {
 
     void likeBlog(Long id);
+
+    List<Blog> getHotBlogs(Integer current);
 }
--- src/main/java/com/hmdp/service/impl/BlogServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/BlogServiceImpl.java
@@ -1,16 +1,22 @@
 package com.hmdp.service.impl;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.hmdp.entity.Blog;
+import com.hmdp.entity.User;
 import com.hmdp.mapper.BlogMapper;
 import com.hmdp.service.IBlogService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.service.IUserService;
 import com.hmdp.utils.RedisConstants;
+import com.hmdp.utils.SystemConstants;
 import com.hmdp.utils.UserHolder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 /**
  * <p>
@@ -26,11 +32,14 @@ public class BlogServiceImpl extends ServiceImpl<BlogMapper, Blog> implements IB
     @Autowired
     private RedisTemplate redisTemplate;
 
+    @Autowired
+    private IUserService userService;
+
     @Override
     @Transactional
     public void likeBlog(Long id) {
         //判断当前登录的用户是否已经点赞过该条blog
-        boolean idLiked = checkIsLiked(id);
+        boolean idLiked = checkIsLiked(UserHolder.getUser().getId(),  id);
         //如果有，提示用户对同一blog点赞多次
         if(idLiked){
             log.warn("用户：{}。已经点赞过该条blog：{}",UserHolder.getUser().getNickName(), id);
@@ -44,14 +53,34 @@ public class BlogServiceImpl extends ServiceImpl<BlogMapper, Blog> implements IB
 
     }
 
+    @Override
+    public List<Blog> getHotBlogs(Integer current) {
+        // 根据用户查询
+        Page<Blog> page = this.query()
+                .orderByDesc("liked")
+                .page(new Page<>(current, SystemConstants.MAX_PAGE_SIZE));
+        // 获取当前页数据
+        List<Blog> records = page.getRecords();
+        // 查询用户
+        records.forEach(blog ->{
+            Long userId = blog.getUserId();
+            User user = userService.getById(userId);
+            blog.setName(user.getNickName());
+            blog.setIsLike(checkIsLiked(UserHolder.getUser().getId(), blog.getId()));
+            blog.setIcon(user.getIcon());
+        });
+
+        return records;
+    }
+
     /**
-     * 检查当前登录用户是否已经点赞过该条blog
-     * @param id
+     * 检查用户是否已经点赞过某条blog
+     * @param
      * @return
      */
-    private boolean checkIsLiked(Long id) {
+    private boolean checkIsLiked(Long userId,Long blogId) {
 
-        return redisTemplate.opsForSet().isMember(RedisConstants.LIKE_BLOG_USERS + id, UserHolder.getUser().getId().toString());
+        return redisTemplate.opsForSet().isMember(RedisConstants.LIKE_BLOG_USERS + blogId, userId.toString());
 
     }
 }
